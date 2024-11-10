21676100da6413641285d34a82ce0d090e03f021
TahmidTausif <tahmidtausif49@gmail.com>
2024-11-10 09:13:35 +0600

    hero updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,29 @@
+import logo from "../assets/images/Group.png"
+import { IoMenu } from "react-icons/io5";
 
 const Header = () => {
     return (
         <div className="fixed z-10 top-0 bg-transparent w-full">
             <div className="navbar z-10 bg-transparent sticky max-w-[1280px] mx-auto">
-                <div className="navbar-start">
+                <div className="navbar-start w-full">
 
-                    <a className="btn btn-ghost text-xl">daisyUI</a>
+                    <a className="btn btn-ghost text-white text-4xl"><img src={logo} alt="" />Restaurent</a>
+                    <div className="hidden pl-10 lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            <li><a className="text-white font-light">Home</a></li>
+                            <li><a className="text-white font-light">About</a></li>
+                            <li><a className="text-white font-light">Portfolio</a></li>
+                            <li><a className="text-white font-light">Client</a></li>
+                            <li><a className="text-white font-light">Blog</a></li>
+                            <li><a className="text-white font-light">Contact</a></li>
+                            
+                        </ul>
+                    </div>
                 </div>
-                <div className="navbar-center hidden lg:flex">
-                    <ul className="menu menu-horizontal px-1">
-                        <li><a>Item 1</a></li>
-                        <li><a>Item 3</a></li>
-                    </ul>
-                </div>
-                <div className="navbar-end">
+                <div className="navbar-end w-[20%]">
                     <div className="dropdown">
                         <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor">
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M4 6h16M4 12h8m-8 6h16" />
-                            </svg>
+                           <IoMenu className="text-white text-4xl"></IoMenu>
                         </div>
                         <ul
                             tabIndex={0}
@@ -38,7 +34,7 @@ const Header = () => {
                         </ul>
 
                     </div>
-                    <a className="btn hidden lg:flex">Button</a>
+                    <button className="text-lg hidden lg:block bg-yellow-400 px-6 py-2 uppercase font-bold">Book a Table</button>
                 </div>
             </div>
         </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,20 +5,20 @@ import vector from "../assets/images/vector_star.png"
 const Hero = () => {
     return (
         <div style={{ backgroundImage: `url(${image})` }} className="flex w-full min-h-[100vh]">
-            <div className="flex lg:flex-row w-[81.25%] mx-auto pt-32 pb-32">
-                <div className="flex flex-col grow relative w-[50%]">
-                    <div className=" mt-10 lg:w-[140%] bg-[linear-gradient(90deg,_#BD1F1700_0%,_#BD1F17B2_70%)] z-50 ">
-                        <h1 className="text-[120px] z-50 uppercase text-white">taste the authentic <br />saudi cuisine</h1>
+            <div className="flex flex-col content-center lg:flex-row w-[81.25%] mx-auto pt-9 pb-20 lg:pt-32 lg:pb-32">
+                <div className="flex flex-col grow relative lg:w-[50%]">
+                    <div className=" mt-20 lg:w-[133%] lg:bg-[linear-gradient(90deg,_#BD1F1700_0%,_#BD1F17B2_70%)] z-50 ">
+                        <h1 className="lg:text-[120px] text-6xl z-50 uppercase text-white">taste the authentic <br />saudi cuisine</h1>
                     </div>
                     <div>
                         <p className="text-white text-2xl py-8">Among the best Saudi chefs in the world, serving you something beyond flavor.</p>
-                        <button className="text-lg bg-yellow-400 px-6 py-4 uppercase font-bold">explore menu</button>
+                        <button className="text-lg bg-yellow-400 px-6 py-4 uppercase font-bold mb-6">explore menu</button>
                     </div>
                 </div>
-                <div className="w-[50%] grow relative">
-                    <img className=" absolute -top-8 -right-8" src={vector} alt="" />
-                    <img className="w-full h-full" src={image2} alt="" />
-                    <img className="absolute -bottom-4 -right-16" src={image3} alt="" />
+                <div className="lg:w-[50%] grow relative">
+                    <img className=" absolute -top-9 -right-7 lg:-right-12" src={vector} alt="" />
+                    <img className="lg:max-w-screen-sm" src={image2} alt="" />
+                    <img className="absolute bottom-2 right-2 lg:-bottom-5 lg:-right-20" src={image3} alt="" />
 
                 </div>
             </div>
